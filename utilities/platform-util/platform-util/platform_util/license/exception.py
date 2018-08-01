1ead900a8f561af857e2eab06ad73c33a92f4c5e
Scott Little <scott.little@windriver.com>
2018-08-01 12:26:18 -0400

    Relocate platform-util to stx-integ/utilities/platform-util
    Move content from stx-utils into stx-integ or stx-update
    
    Packages will be relocated to
    
    stx-update:
        enable-dev-patch
        extras
    
    stx-integ:
        config-files/
            io-scheduler
    
        filesystem/
            filesystem-scripts
    
        grub/
            grubby
    
        logging/
            logmgmt
    
        tools/
            collector
            monitor-tools
    
        tools/engtools/
            hostdata-collectors
            parsers
    
        utilities/
            build-info
            branding   (formerly wrs-branding)
            platform-util
    
    Change-Id: I8ee29b0511085355ebca37fd46afe087fbd00ea8
    Story: 2002801
    Task: 22687
    Signed-off-by: Scott Little <scott.little@windriver.com>

---
+++ utilities/platform-util/platform-util/platform_util/license/exception.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2018 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+class ValidateError(Exception):
+    """Base class for license validation exceptions."""
+
+    def __init__(self, message=None):
+        self.message = message
+
+    def __str__(self):
+        return self.message or ""
+
+
+class InvalidLicense(ValidateError):
+    """Generic invalid license error"""
+    pass
+
+
+class ExpiredLicense(ValidateError):
+    """Expired license error"""
+    pass
+
+
+class InvalidLicenseVersion(ValidateError):
+    """Invalid license version error"""
+    pass
+
+
+class InvalidLicenseType(ValidateError):
+    """Invalid license type error"""
+    pass
+
+
+class LicenseNotFound(ValidateError):
+    """License not found error"""
+    pass
