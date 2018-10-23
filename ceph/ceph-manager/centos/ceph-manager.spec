openstack-archive/stx-integ

Add hooks for python wheel generation
This update adds hooks to the spec files for the following packages
to generate wheels for the python modules:
- ceph-manager
- libvirt-python
- logmgmt
- platform-util
- python-3parclient
- python-cephclient
- python-lefthandclient
- python-ryu
- vm-topology

Change-Id: Ia63291e686818d19d0df52ff26b5f0bb3812b8ce
Story: 2003907
Task: 26787
Signed-off-by: Don Penney <don.penney@windriver.com>

ceph/ceph-manager/centos/ceph-manager.spec
@@ -9,6 +9,8 @@ URL: unknown
 Source0: %{name}-%{version}.tar.gz
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 BuildRequires: systemd-units
 BuildRequires: systemd-devel
 Requires: sysinv
@@ -31,6 +33,7 @@ Handle sysinv RPC calls for long running Ceph API operations:
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -38,6 +41,8 @@ Handle sysinv RPC calls for long running Ceph API operations:
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_etc_initd}
 install -p -D -m 700 scripts/init.d/ceph-manager %{buildroot}%{local_etc_initd}/ceph-manager
@@ -68,3 +73,12 @@ rm -rf $RPM_BUILD_ROOT
 %{pythonroot}/ceph_manager/*
 %dir %{pythonroot}/ceph_manager-%{version}.0-py2.7.egg-info
 %{pythonroot}/ceph_manager-%{version}.0-py2.7.egg-info/*
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

ceph/python-cephclient/centos/python-cephclient.spec
@@ -25,6 +25,8 @@ BuildArch: noarch
 
 BuildRequires: python
 BuildRequires: ceph
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 
 Requires: python
 
@@ -44,9 +46,12 @@ rm -f requirements.txt
 
 %build
 %{__python2} setup.py build
+%py2_build_wheel
 
 %install
 %{__python2} setup.py install --skip-build --root %{buildroot}
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %files
 %doc README.rst
@@ -54,3 +59,11 @@ rm -f requirements.txt
 %{python2_sitelib}/cephclient
 %{python2_sitelib}/*.egg-info
 
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

logging/logmgmt/centos/logmgmt.spec
@@ -10,6 +10,8 @@ Source0: %{name}-%{version}.tar.gz
 Source1: LICENSE
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 BuildRequires: systemd-devel
 Requires: systemd
 Requires: python-daemon
@@ -32,6 +34,7 @@ rm -rf *.egg-info
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -39,6 +42,8 @@ rm -rf *.egg-info
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_bindir}
 install -p -D -m 700 scripts/bin/logmgmt %{buildroot}%{local_bindir}/logmgmt
@@ -71,3 +76,12 @@ rm -rf $RPM_BUILD_ROOT
 %{pythonroot}/%{name}/*
 %dir %{pythonroot}/%{name}-%{version}.0-py2.7.egg-info
 %{pythonroot}/%{name}-%{version}.0-py2.7.egg-info/*
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

networking/python-ryu/centos/python-ryu.spec
@@ -41,6 +41,8 @@ Requires:  python-webob
 Requires:  python-%{pypi_name}-common = %{version}-%{release}
 
 BuildRequires:  python2-devel
+BuildRequires:  python2-pip
+BuildRequires:  python2-wheel
 BuildRequires:  python-debtcollector
 BuildRequires:  python-eventlet
 BuildRequires:  python-greenlet
@@ -153,6 +155,7 @@ rm -rf %{pypi_name}/tests/integrated/common
 %build
 export PBR_VERSION=%{version}
 %py2_build
+%py2_build_wheel
 %if 0%{?with_python3}
 %py3_build
 %endif
@@ -174,6 +177,8 @@ for bin in %{pypi_name}{,-manager}; do
     ln -s ./$bin-%{python2_version} %{buildroot}%{_bindir}/$bin-2
     ln -s ./$bin-%{python2_version} %{buildroot}%{_bindir}/$bin
 done;
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 
 install -d -m 755 %{buildroot}%{_sysconfdir}/%{pypi_name}
@@ -223,6 +228,15 @@ PYTHON=%{__python2} ./run_tests.sh -N -P
 %files -n python-%{pypi_name}-tests
 %license LICENSE
 %{python2_sitelib}/%{pypi_name}/tests
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*
 
 %changelog
 * Tue Jul 04 2017 Matt Peters <matt.peters@windriver.com> - 4.15-0

storage-drivers/python-3parclient/centos/python-3parclient.spec
@@ -15,6 +15,8 @@ Url: http://packages.python.org/python-3parclient
 
 BuildRequires:    python2-devel
 BuildRequires:    python-setuptools
+BuildRequires:    python2-pip
+BuildRequires:    python2-wheel
 
 %description
 HPE 3PAR HTTP REST Client
@@ -24,12 +26,24 @@ HPE 3PAR HTTP REST Client
 
 %build
 %{__python2} setup.py build
+%py2_build_wheel
 
 %install
 %{__python2} setup.py install -O1 --skip-build --root %{buildroot} --record=INSTALLED_FILES
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %clean
 rm -rf %{buildroot}
 
 %files -f INSTALLED_FILES
 %defattr(-,root,root)
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

storage-drivers/python-lefthandclient/centos/python-lefthandclient.spec
@@ -17,6 +17,8 @@ Url: http://packages.python.org/python-lefthandclient
 
 BuildRequires:    python2-devel
 BuildRequires:    python-setuptools
+BuildRequires:    python2-pip
+BuildRequires:    python2-wheel
 
 %description
 HPE LeftHand/StoreVirtual HTTP REST Client
@@ -26,12 +28,24 @@ HPE LeftHand/StoreVirtual HTTP REST Client
 
 %build
 %{__python2} setup.py build
+%py2_build_wheel
 
 %install
 %{__python2} setup.py install -O1 --skip-build --root %{buildroot} --record=INSTALLED_FILES
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %clean
 rm -rf %{buildroot}
 
 %files -f INSTALLED_FILES
 %defattr(-,root,root)
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

utilities/platform-util/centos/platform-util.spec
@@ -10,6 +10,8 @@ BuildArch: noarch
 Source: %name-%version.tar.gz
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 
 %description
 Platform utilities
@@ -36,6 +38,7 @@ Platform utilities that packaged on controllers or one node system
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -43,6 +46,8 @@ Platform utilities that packaged on controllers or one node system
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %global _buildsubdir %{_builddir}/%{name}-%{version}
 install -d %{buildroot}%{local_bindir}
@@ -96,3 +101,12 @@ systemctl enable opt-platform.service
 %files -n platform-util-controller
 %defattr(-,root,root,-)
 /etc/systemd/system/memcached.service
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

starlingx/integ

ceph/ceph-manager/centos/ceph-manager.spec
@@ -9,6 +9,8 @@ URL: unknown
 Source0: %{name}-%{version}.tar.gz
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 BuildRequires: systemd-units
 BuildRequires: systemd-devel
 Requires: sysinv
@@ -31,6 +33,7 @@ Handle sysinv RPC calls for long running Ceph API operations:
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -38,6 +41,8 @@ Handle sysinv RPC calls for long running Ceph API operations:
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_etc_initd}
 install -p -D -m 700 scripts/init.d/ceph-manager %{buildroot}%{local_etc_initd}/ceph-manager
@@ -68,3 +73,12 @@ rm -rf $RPM_BUILD_ROOT
 %{pythonroot}/ceph_manager/*
 %dir %{pythonroot}/ceph_manager-%{version}.0-py2.7.egg-info
 %{pythonroot}/ceph_manager-%{version}.0-py2.7.egg-info/*
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*

ceph/python-cephclient/centos/python-cephclient.spec
@@ -25,6 +25,8 @@ BuildArch: noarch
 
 BuildRequires: python
 BuildRequires: ceph
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 
 Requires: python
 
@@ -44,9 +46,12 @@ rm -f requirements.txt
 
 %build
 %{__python2} setup.py build
+%py2_build_wheel
 
 %install
 %{__python2} setup.py install --skip-build --root %{buildroot}
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %files
 %doc README.rst
@@ -54,3 +59,11 @@ rm -f requirements.txt
 %{python2_sitelib}/cephclient
 %{python2_sitelib}/*.egg-info
 
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*

logging/logmgmt/centos/logmgmt.spec
@@ -10,6 +10,8 @@ Source0: %{name}-%{version}.tar.gz
 Source1: LICENSE
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 BuildRequires: systemd-devel
 Requires: systemd
 Requires: python-daemon
@@ -32,6 +34,7 @@ rm -rf *.egg-info
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -39,6 +42,8 @@ rm -rf *.egg-info
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_bindir}
 install -p -D -m 700 scripts/bin/logmgmt %{buildroot}%{local_bindir}/logmgmt
@@ -71,3 +76,12 @@ rm -rf $RPM_BUILD_ROOT
 %{pythonroot}/%{name}/*
 %dir %{pythonroot}/%{name}-%{version}.0-py2.7.egg-info
 %{pythonroot}/%{name}-%{version}.0-py2.7.egg-info/*
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*

networking/python-ryu/centos/python-ryu.spec
@@ -41,6 +41,8 @@ Requires:  python-webob
 Requires:  python-%{pypi_name}-common = %{version}-%{release}
 
 BuildRequires:  python2-devel
+BuildRequires:  python2-pip
+BuildRequires:  python2-wheel
 BuildRequires:  python-debtcollector
 BuildRequires:  python-eventlet
 BuildRequires:  python-greenlet
@@ -153,6 +155,7 @@ rm -rf %{pypi_name}/tests/integrated/common
 %build
 export PBR_VERSION=%{version}
 %py2_build
+%py2_build_wheel
 %if 0%{?with_python3}
 %py3_build
 %endif
@@ -174,6 +177,8 @@ for bin in %{pypi_name}{,-manager}; do
     ln -s ./$bin-%{python2_version} %{buildroot}%{_bindir}/$bin-2
     ln -s ./$bin-%{python2_version} %{buildroot}%{_bindir}/$bin
 done;
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 
 install -d -m 755 %{buildroot}%{_sysconfdir}/%{pypi_name}
@@ -223,6 +228,15 @@ PYTHON=%{__python2} ./run_tests.sh -N -P
 %files -n python-%{pypi_name}-tests
 %license LICENSE
 %{python2_sitelib}/%{pypi_name}/tests
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*
 
 %changelog
 * Tue Jul 04 2017 Matt Peters <matt.peters@windriver.com> - 4.15-0

storage-drivers/python-3parclient/centos/python-3parclient.spec
@@ -15,6 +15,8 @@ Url: http://packages.python.org/python-3parclient
 
 BuildRequires:    python2-devel
 BuildRequires:    python-setuptools
+BuildRequires:    python2-pip
+BuildRequires:    python2-wheel
 
 %description
 HPE 3PAR HTTP REST Client
@@ -24,12 +26,24 @@ HPE 3PAR HTTP REST Client
 
 %build
 %{__python2} setup.py build
+%py2_build_wheel
 
 %install
 %{__python2} setup.py install -O1 --skip-build --root %{buildroot} --record=INSTALLED_FILES
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %clean
 rm -rf %{buildroot}
 
 %files -f INSTALLED_FILES
 %defattr(-,root,root)
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*

storage-drivers/python-lefthandclient/centos/python-lefthandclient.spec
@@ -17,6 +17,8 @@ Url: http://packages.python.org/python-lefthandclient
 
 BuildRequires:    python2-devel
 BuildRequires:    python-setuptools
+BuildRequires:    python2-pip
+BuildRequires:    python2-wheel
 
 %description
 HPE LeftHand/StoreVirtual HTTP REST Client
@@ -26,12 +28,24 @@ HPE LeftHand/StoreVirtual HTTP REST Client
 
 %build
 %{__python2} setup.py build
+%py2_build_wheel
 
 %install
 %{__python2} setup.py install -O1 --skip-build --root %{buildroot} --record=INSTALLED_FILES
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %clean
 rm -rf %{buildroot}
 
 %files -f INSTALLED_FILES
 %defattr(-,root,root)
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*

utilities/platform-util/centos/platform-util.spec
@@ -10,6 +10,8 @@ BuildArch: noarch
 Source: %name-%version.tar.gz
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 
 %description
 Platform utilities
@@ -36,6 +38,7 @@ Platform utilities that packaged on controllers or one node system
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -43,6 +46,8 @@ Platform utilities that packaged on controllers or one node system
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 %global _buildsubdir %{_builddir}/%{name}-%{version}
 install -d %{buildroot}%{local_bindir}
@@ -96,3 +101,12 @@ systemctl enable opt-platform.service
 %files -n platform-util-controller
 %defattr(-,root,root,-)
 /etc/systemd/system/memcached.service
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*