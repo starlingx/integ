openstack-archive/stx-integ

ceph: add mgr-restful-plugin service
ceph-rest-api was removed in Ceph Mimic.  Similar functionality is
provided by ceph-mgr restful plugin. This new service needs to be
configured and started instead of ceph-rest-api:

* enable mgr-restful-plugin in ceph.conf

* add mgr-restrul-plugin service file

* add mgr-restful-plugin monitoring daemon
  waiting for ceph to become available before
  it configures and starts ceph-mgr daemon
  providing RESTful API endpoints

Story: 2003605
Task: 28860

Depends-On: Iaa3319a7647e5622037d12c53673da0e4199ceb4
Change-Id: I31fb9aac89c44bbce24939197446caa987d395cb
Co-Authored-By: Yong Hu <yong.hu@intel.com>
Co-Authored-By: Changcheng Liu <changcheng.liu@intel.com>
Signed-off-by: Daniel Badea <daniel.badea@windriver.com>

ceph/ceph/centos/ceph.spec
@@ -271,7 +271,7 @@ URL:		http://ceph.com/
 Source0:	%{?_remote_tarball_prefix}ceph-13.2.2.tar.gz
 
 Source1:  ceph.sh
-Source2:  mgr-restful-plugin
+Source2:  mgr-restful-plugin.py
 Source3:  ceph.conf.pmon
 Source4:  ceph-init-wrapper.sh
 Source5:  ceph.conf
@@ -1237,7 +1237,7 @@ mkdir -p %{buildroot}%{_unitdir}
 install -m 750 %{SOURCE1} %{buildroot}%{_sysconfdir}/services.d/controller/
 install -m 750 %{SOURCE1} %{buildroot}%{_sysconfdir}/services.d/storage/
 install -m 750 %{SOURCE1} %{buildroot}%{_sysconfdir}/services.d/worker/
-install -m 750 %{SOURCE2} %{buildroot}%{_initrddir}/
+install -m 750 %{SOURCE2} %{buildroot}%{_initrddir}/mgr-restful-plugin
 install -m 750 %{SOURCE3} %{buildroot}%{_sysconfdir}/ceph/
 install -m 750 %{SOURCE4} %{buildroot}%{_initrddir}/ceph-init-wrapper
 install -m 640 %{SOURCE5} %{buildroot}%{_sysconfdir}/ceph/

starlingx/integ

ceph/ceph/centos/ceph.spec
@@ -290,7 +290,7 @@ Source17:	src/spdk/dpdk/dpdk-6ece49ad5a26f5e2f5c4af6c06c30376c0ddc387.tar.gz
 Source18:	src/rapidjson/thirdparty/gtest/googletest-0a439623f75c029912728d80cb7f1b8b48739ca4.tar.gz
 
 Source19:	ceph.sh
-Source20:	mgr-restful-plugin
+Source20:	mgr-restful-plugin.py
 Source21:	ceph.conf.pmon
 Source22:	ceph-init-wrapper.sh
 Source23:	ceph.conf
@@ -1292,7 +1292,7 @@ mkdir -p %{buildroot}%{_unitdir}
 install -m 750 %{SOURCE19} %{buildroot}%{_sysconfdir}/services.d/controller/
 install -m 750 %{SOURCE19} %{buildroot}%{_sysconfdir}/services.d/storage/
 install -m 750 %{SOURCE19} %{buildroot}%{_sysconfdir}/services.d/worker/
-install -m 750 %{SOURCE20} %{buildroot}%{_initrddir}/
+install -m 750 %{SOURCE20} %{buildroot}%{_initrddir}/mgr-restful-plugin
 install -m 750 %{SOURCE21} %{buildroot}%{_sysconfdir}/ceph/
 install -m 750 %{SOURCE22} %{buildroot}%{_initrddir}/ceph-init-wrapper
 install -m 640 %{SOURCE23} %{buildroot}%{_sysconfdir}/ceph/